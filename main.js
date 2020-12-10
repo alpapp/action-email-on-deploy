a5b1df6e699a6d08129c43241226908d561f6e94
Chris <chris@SFLiMac.broadband>
2020-12-10 16:06:25 +0000

    branch test

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ async function main() {
         // now get branch we deployed on
         // const deployedBranch = (await execShellCommand('git name-rev --name-only --exclude=tags/* ' + process.env.GITHUB_SHA)).trim()
 
-        const deployedBranch = (await execShellCommand(/*"git log --graph --pretty='%D' --date=short -1"*/ "git log -1")).trim()
+        const deployedBranch = (await execShellCommand(/*"git log --graph --pretty='%D' --date=short -1"*/ "git branch --sort=-committerdate")).trim()
         console.log('deployedBranch is ' + deployedBranch)
 
         // console.dir(github.context, {depth: null})
